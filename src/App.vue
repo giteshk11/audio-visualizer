90af0af58ca883a5da54ccd1c8955adbebcc3daa
Gitesh Khurani <giteshkhurani@Giteshs-MacBook-Pro.local>
2022-02-21 20:57:35 +0530

    Changed audioAnalyser file to class based syntax

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
-    <Renderer ref="rendererC" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
-        <Camera :position="{ z: 10 }" />
-        <Scene>
-            <PointLight :position="{ y: 50, z: 50 }" :color="'red'" />
-            <Box
-                :size="2"
-                ref="meshC"
-                :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }"
-                @click="startSong()"
-            >
-                <LambertMaterial />
-            </Box>
-        </Scene>
-    </Renderer>
+  <Renderer
+    ref="rendererC"
+    antialias
+    :orbit-ctrl="{ enableDamping: true }"
+    resize="window"
+  >
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" :color="'red'" />
+      <Box
+        :size="2"
+        ref="meshC"
+        :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }"
+        @click="startSong"
+      >
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { loadSong, analyseAudio, loadAudioContext, setupAnalyser } from './audioAnalyser';
-import {
+  import { ref, onMounted } from "vue";
+  import AudioAnalyser from "./audioAnalyser";
+  import {
     Box,
     Camera,
     LambertMaterial,
@@ -27,39 +32,38 @@ import {
     Renderer,
     RendererPublicInterface,
     Scene,
-} from 'troisjs';
+  } from "troisjs";
+  import excuses from "../assets/input.mp3";
 
-const rendererC = ref();
-const meshC = ref();
-const audio = loadSong();
-const { source, audioCtx } = loadAudioContext(audio);
-const analyser = setupAnalyser({ source, audioCtx });
-analyseAudio(analyser);
-const isPlaying = ref(true);
+  const rendererC = ref();
+  const meshC = ref();
+  const isPlaying = ref(true);
+  const audioAnalyser = new AudioAnalyser(excuses);
+  const analyser = audioAnalyser.setupAnalyser();
+  audioAnalyser.analyseAudio(analyser);
 
-onMounted(() => {
+  onMounted(() => {
     const renderer = rendererC.value as RendererPublicInterface;
     const mesh = (meshC.value as MeshPublicInterface).mesh;
     renderer.onBeforeRender(() => {
-        mesh!.rotation.x += 0.01;
-        analyseAudio(analyser);
+      mesh!.rotation.x += 0.01;
+      console.log(audioAnalyser.analyseAudio(analyser));
     });
-});
+  });
 
-async function startSong() {
-    await audioCtx.resume();
-    isPlaying.value ? audio.play() : audio.pause();
+  async function startSong() {
+    await audioAnalyser.audioCtx.resume();
+    isPlaying.value ? audioAnalyser.audio.play() : audioAnalyser.audio.pause();
     isPlaying.value = !isPlaying.value;
-}
+  }
 </script>
-xw
 
 <style>
-xw body,
-html {
+  body,
+  html {
     margin: 0;
-}
-canvas {
+  }
+  canvas {
     display: block;
-}
+  }
 </style>
--- src/audioAnalyser.ts
+++ src/audioAnalyser.ts
@@ -1,30 +1,42 @@
-import excuses from '../assets/input.mp3';
-import { AnalyserParams } from './types';
+import { AnalyserParams } from "./types";
 
-export function loadSong() {
-    const audio = new Audio(excuses);
-    return audio;
-}
+export default class AudioAnalyser {
+  private _audio;
+  private _audioCtx!: AudioContext;
+  private _source!: MediaElementAudioSourceNode;
+  constructor(song: string) {
+    this._audio = new Audio(song);
+    this.loadAudioContext();
+  }
 
-export function loadAudioContext(audio: HTMLAudioElement): AnalyserParams {
-    const audioCtx = new AudioContext();
+  get audioCtx() {
+    return this._audioCtx;
+  }
 
-    const source = audioCtx.createMediaElementSource(audio);
-    return { audioCtx, source };
-}
+  get audio() {
+    return this._audio;
+  }
 
-export function setupAnalyser(params: AnalyserParams) {
-    const { audioCtx, source } = params;
-    const analyser = audioCtx.createAnalyser();
-    analyser.connect(audioCtx.destination);
-    source.connect(analyser);
-    analyser.fftSize = 512;
+  loadAudioContext() {
+    if (this.audio) {
+      this._audioCtx = new AudioContext();
+      this._source = this.audioCtx.createMediaElementSource(this.audio);
+    }
+  }
+  setupAnalyser(): AnalyserNode {
+    let analyser!: AnalyserNode;
+    if (this._audioCtx && this._source) {
+      analyser = this.audioCtx.createAnalyser();
+      analyser.connect(this.audioCtx.destination);
+      this._source.connect(analyser);
+      return analyser;
+    }
     return analyser;
-}
-
-export function analyseAudio(analyser: AnalyserNode) {
+  }
+  analyseAudio(analyser: AnalyserNode): Uint8Array {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
     analyser.getByteFrequencyData(dataArray);
-    console.log(dataArray[0]);
+    return dataArray;
+  }
 }
